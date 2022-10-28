ddb21edcc70b60a980c08a76c373674271d04368
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-10-28 07:11:19 +0600

    Add files via upload

--- Lab09/Test/ResultPublication/ResultPublicationTest.java
+++ Lab09/Test/ResultPublication/ResultPublicationTest.java
@@ -67,7 +67,7 @@ public class ResultPublicationTest {
 
     @Test
     public void TestCSVWrite() throws InvalidStudentIDException {
-        Student student1 = new Student("123456787","Rahim","SWE",3.99f);
+        Student student1 = new Student("123456787","Rahim","SWE",4.00f);
         Student student2 = new Student("123456789","Adam","SWE",4.00f);
         Student student3 = new Student("123456780","Barkat","SWE",3.00f);
         StudentEnrollment studentEnrollment=new StudentEnrollment();
@@ -80,6 +80,39 @@ public class ResultPublicationTest {
         assertEquals(export.readExportedFile(),"123456787,Rahim,SWE,4.00\n" +
                 "123456789,Adam,SWE,4.00\n" +
                 "123456780,Barkat,SWE,3.00\n");
-
     }
+
+    @Test
+    public void TestXMLWrite() throws InvalidStudentIDException{
+        Student student1 = new Student("123456787","Rahim","SWE",4.00f);
+        Student student2 = new Student("123456789","Adam","SWE",4.00f);
+        Student student3 = new Student("123456780","Barkat","SWE",3.00f);
+        StudentEnrollment studentEnrollment=new StudentEnrollment();
+        studentEnrollment.add(student1);
+        studentEnrollment.add(student2);
+        studentEnrollment.add(student3);
+        ResultPublication rps=new ResultPublication(studentEnrollment.studentList);
+        ExportXML export=new ExportXML("file.xml");
+        assertTrue(rps.ExportData(export));
+        assertEquals(export.readExportedFile(),"<Student>\n" +
+                "<studentID>123456787</studentID>\n" +
+                "<name>Rahim</name>\n" +
+                "<program>SWE</program>\n" +
+                "<CGPA>4.00</CGPA>\n" +
+                "</Student>\n" +
+                "<Student>\n" +
+                "<studentID>123456789</studentID>\n" +
+                "<name>Adam</name>\n" +
+                "<program>SWE</program>\n" +
+                "<CGPA>4.00</CGPA>\n" +
+                "</Student>\n" +
+                "<Student>\n" +
+                "<studentID>123456780</studentID>\n" +
+                "<name>Barkat</name>\n" +
+                "<program>SWE</program>\n" +
+                "<CGPA>3.00</CGPA>\n" +
+                "</Student>\n");
+    }
+
+
 }
