32ac327ab8a0233bc3a0d7a5b3df5fb579ecfe3c
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-11-18 16:58:24 +0600

    Finishing the assignment

--- Assignment01/src/main/java/org/example/Main.java
+++ Assignment01/src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
-import assignment2.ContentCoupling.Calculator;
-import assignment2.ContentCoupling.Numbers;
+import assignment2.Independence.Calculator;
+import assignment2.Independence.Numbers;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calculator calculator=new Calculator();
+        System.out.println(calculator.Sum(2,3));
     }
 }
